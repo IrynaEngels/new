2594b767bc6dfeca4d036a3ddeab9f8de4c63639
IrynaEngels <engelsiryna@gmail.com>
2017-07-23 13:36:04 +0300

    init

---
+++ app/src/main/java/com/example/kapusta/buttons/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.kapusta.buttons;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button black = (Button) findViewById(R.id.blackbutton);
+    }
+}
